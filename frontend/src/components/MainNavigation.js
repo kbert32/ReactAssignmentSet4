0f47c2116146fd01de7b042da25f12bd7c56c0d0
kbert32 <103608624+kbert32@users.noreply.github.com>
2023-03-02 09:33:53 -0700

    Update MainNavigation.js

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,14 +1,14 @@
 import { NavLink } from 'react-router-dom';
 import classes from './MainNavigation.module.css';
 
-function MainNavigation() {
-  return (
+function MainNavigation() {                     //for some reason the 'Home' NavLink is working properly 
+  return (                                      //although it should contain the 'end' property
     <header className={classes.header}>
       <nav>
         <ul className={classes.list}>
           <li>
             <NavLink to="/" className={({isActive}) =>
-              isActive ? classes.active : undefined}>Home</NavLink>
+              isActive ? classes.active : undefined}>Home</NavLink>   
           </li>
           <li>
             <NavLink to="/events" className={({isActive}) =>
