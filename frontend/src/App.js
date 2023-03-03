728fb9951d953b26a8deb49814f829268c6a73e0
kbert32 <103608624+kbert32@users.noreply.github.com>
2023-03-02 17:35:27 -0700

    20 adv

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 import HomePage from "./pages/Home";
-import EventsPage from "./pages/Events";
+import EventsPage, {loader as eventsLoader} from "./pages/Events";
 import EventDetailPage from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventLayout from "./pages/EventLayout";
+import ErrorPage from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {index: true, element: <HomePage />},
       {
         path: 'events',
         element: <EventLayout />,
         children: [
-          {index: true, element: <EventsPage />},
+          {index: true, element: <EventsPage />, loader: eventsLoader},
           {path: ':eventId', element: <EventDetailPage />},
           {path: 'new', element: <NewEventPage />},
           {path: ':editId/edit', element: <EditEventPage />}
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,23 +1,29 @@
-import { Link } from "react-router-dom";
+import { useLoaderData } from 'react-router-dom';
 
-const EVENTS = [
-    {id: 'e1', title: 'Event 1'},
-    {id: 'e2', title: 'Event 2'},
-    {id: 'e3', title: 'Event 3'},
-];
+import EventsList from '../components/EventsList';
 
 function EventsPage() {
-    return (
-        <>
-            <h1>Events</h1>
-            <ul>
-                {EVENTS.map((event) =>
-                    <li key={event.id}>
-                        <Link to={event.id}>{event.title}</Link>
-                    </li>)}
-            </ul>
-        </>
-    );
+    const data = useLoaderData();
+    const events = data.events;
+
+    return <EventsList events={events} />;
 };
 
 export default EventsPage;
+
+export async function loader() {
+    const response = await fetch('http://localhost:8080/eventss');
+
+    if (!response.ok) {
+        // throw new Response(JSON.stringify({message: 'Could not fetch events.'}), {status: 500});
+        throw new Response('haha', {status: 500, statusText: 'Could not fetch events.'});
+    } else {
+        // const resData = await response.json();   //these statements are not needed with react router
+        // return resData.events;                   //react router returns the data part of the Response constructor
+        return response;                            //we can return the 'response' directly
+    }
+};
+
+//any browser API's can be executed within the loader function, for example: access local storage, 
+//access cookies, you can do anything you can do in normal javascript code.
+//What you cannot do is use React hooks because this is not a react component
