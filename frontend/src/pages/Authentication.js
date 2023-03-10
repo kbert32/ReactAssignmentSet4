f37eea2d8b9bd1b93979787d96d09dbaee99fc5b
kbert32 <103608624+kbert32@users.noreply.github.com>
2023-03-10 15:57:30 -0700

    21

---
+++ frontend/src/pages/Authentication.js
@@ -0,0 +1,20 @@
+import AuthForm from '../components/AuthForm';
+
+function AuthenticationPage() {
+  return <AuthForm />;
+}
+
+export default AuthenticationPage;
+
+export async function action ({request}) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get('mode') || 'login';
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password')
+  };
+
+  fetch('http://localhost8080/' + mode);
+};
