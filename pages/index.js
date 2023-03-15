284461f54e3e258147e6e00e4915e43bde2496c1
kbert32 <103608624+kbert32@users.noreply.github.com>
2023-03-15 17:19:09 -0600

    23 meetups

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+const MEETUPS = [
+    {
+        id: 'm1',
+        title: 'A First Meetup',
+        address: 'Some address 5, 12345 Some City',
+        description: 'This is a first meetup!',
+    },
+    {
+        id: 'm2',
+        title: 'A Second Meetup',
+        address: 'Some address 10, 12345 Some City',
+        description: 'This is a second meetup!',
+    },
+]
+
+export default function HomePage() {
+    return <MeetupList meetup={MEETUPS} />
+};
