44cad57d7171365071e7e547d5bef963da493bf7
kbert32 <103608624+kbert32@users.noreply.github.com>
2023-03-15 17:45:39 -0600

    23 meetups

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,19 @@ const MEETUPS = [
     {
         id: 'm1',
         title: 'A First Meetup',
-        address: 'Some address 5, 12345 Some City',
-        description: 'This is a first meetup!',
+        image: 'https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg',
+        addres: 'Some address 5, 12345 Some City',
+        description: 'This is a first meetup!'
     },
     {
         id: 'm2',
         title: 'A Second Meetup',
-        address: 'Some address 10, 12345 Some City',
-        description: 'This is a second meetup!',
+        image: 'https://www.planetware.com/wpimages/2019/11/india-best-places-to-visit-agra.jpg',
+        addres: 'Some address 10, 12345 Some City',
+        description: 'This is a second meetup!'
     },
-]
+];
 
 export default function HomePage() {
-    return <MeetupList meetup={MEETUPS} />
+    return <MeetupList meetups={MEETUPS} />
 };
