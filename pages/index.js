bf2de3767846726b8f37685ac7d20c1622c94521
kbert32 <103608624+kbert32@users.noreply.github.com>
2023-03-17 15:57:44 -0600

    23

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,24 @@ const DUMMY_MEETUPS = [
 
 const MEETUPS = DUMMY_MEETUPS;
 
-export default function HomePage() {
-    return <MeetupList meetups={MEETUPS} />
+export default function HomePage(props) {   //props received here comes from the 'getStaticProps' function
+
+    return <MeetupList meetups={props.meetups} />;
 };
+
+    //getStaticProps is a reserved name, Next.js looks for this name, and if it finds it, it executes this function
+    //before pre-rendering the component. The 'getStaticProps' function must be declared within the component.  
+    //It can run asynchronously so it returns a promise, Next.js will wait for the promise to resolve  
+    //before executing the component function, therefore you're able to load data before the component is rendered 
+    //You can execute code in this function that would normally run on a server, (access a file system, securely connect
+    //to a database or API), this function is only executed during the build process, not on the server or on the client side
+    //
+export async function getStaticProps() {    //this function must return an object, typically with a 'props' property
+    //fetch data from an API
+    return {
+        props: {
+            meetups: DUMMY_MEETUPS, 
+            revalidate: 10              //revalidate unlocks a feature called incremental static regeneration, this page will 
+        }                               //be re-generated on the server every 10 seconds if there are requests coming in for this page
+    };                                  //ensures that the page will re pre-generate on the server after deployment to avoid having to
+};                                      //redeploy and rebuild just because some data changed
